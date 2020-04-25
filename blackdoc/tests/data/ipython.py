519284f30fc32385ca98f41855d50319f7563dca
Keewis <keewis@posteo.de>
2020-04-25 12:34:11 +0200

    add a newline to the ipython-style docstring

--- blackdoc/tests/data/ipython.py
+++ blackdoc/tests/data/ipython.py
@@ -6,6 +6,7 @@ docstring = """ a function to open files
        ...:    "very_long_filepath",
        ...:    mode="a",
        ...: )
+
     In [2]: file
     Out[2]: <_io.TextIOWrapper name='very_long_filepath' mode='w' encoding='UTF-8'>
 
@@ -16,7 +17,24 @@ docstring = """ a function to open files
     Out[3]: False
 """
 lines = docstring.split("\n")
-code_units = (1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+code_units = (1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+line_ranges = (
+    (0, 1),
+    (1, 2),
+    (2, 3),
+    (3, 4),
+    (4, 8),
+    (8, 9),
+    (9, 10),
+    (10, 11),
+    (11, 12),
+    (12, 13),
+    (13, 14),
+    (14, 15),
+    (15, 16),
+    (16, 17),
+    (17, 18),
+)
 line_labels = (
     "none",
     "none",
@@ -26,6 +44,7 @@ line_labels = (
     "ipython",
     "ipython",
     "ipython",
+    "none",
     "ipython",
     "none",
     "none",
