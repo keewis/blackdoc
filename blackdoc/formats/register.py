007c1ef734dd26d10ccc4de95fcbf4e43c456851
Keewis <keewis@posteo.de>
2020-05-21 16:37:25 +0200

    add machinery to register include regexes

--- blackdoc/formats/register.py
+++ blackdoc/formats/register.py
@@ -3,6 +3,7 @@ import warnings
 detection_funcs = {}
 extraction_funcs = {}
 reformatting_funcs = {}
+include_patterns = {}
 
 
 def register_format(name, obj):
@@ -13,7 +14,11 @@ def register_format(name, obj):
     detection_func = getattr(obj, "detection_func")
     extraction_func = getattr(obj, "extraction_func")
     reformatting_func = getattr(obj, "reformatting_func")
+    include_pattern = getattr(obj, "include_pattern", None)
 
     detection_funcs[name] = detection_func
     extraction_funcs[name] = extraction_func
     reformatting_funcs[name] = reformatting_func
+
+    if include_pattern is not None:
+        include_patterns[name] = include_pattern
