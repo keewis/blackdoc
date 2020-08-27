fad572bd679d9e7ae59700dff73a7967e6bf793e
Keewis <keewis@posteo.de>
2020-08-27 15:39:38 +0200

    vendor gen_python_files

--- blackdoc/__main__.py
+++ blackdoc/__main__.py
@@ -7,6 +7,80 @@ import black
 from . import __version__, format_lines, formats
 
 
+def normalize_path_maybe_ignore(path, root, report):
+    """Normalize `path`. May return `None` if `path` was ignored.
+    `report` is where "path ignored" output goes.
+    """
+    try:
+        abspath = path if path.is_absolute() else pathlib.Path.cwd() / path
+        normalized_path = abspath.resolve().relative_to(root).as_posix()
+    except OSError as e:
+        report.path_ignored(path, f"cannot be read because {e}")
+        return None
+
+    except ValueError:
+        if path.is_symlink():
+            report.path_ignored(path, f"is a symbolic link that points outside {root}")
+            return None
+
+        raise
+
+    return normalized_path
+
+
+def gen_python_files(paths, root, include, exclude, force_exclude, report, gitignore):
+    """Generate all files under `path` whose paths are not excluded by the
+    `exclude_regex` or `force_exclude` regexes, but are included by the `include` regex.
+
+    Symbolic links pointing outside of the `root` directory are ignored.
+
+    `report` is where output about exclusions goes.
+    """
+    assert root.is_absolute(), f"INTERNAL ERROR: `root` must be absolute but is {root}"
+    for child in paths:
+        normalized_path = normalize_path_maybe_ignore(child, root, report)
+        if normalized_path is None:
+            continue
+
+        # First ignore files matching .gitignore
+        if gitignore.match_file(normalized_path):
+            report.path_ignored(child, "matches the .gitignore file content")
+            continue
+
+        # Then ignore with `--exclude` and `--force-exclude` options.
+        normalized_path = "/" + normalized_path
+        if child.is_dir():
+            normalized_path += "/"
+
+        exclude_match = exclude.search(normalized_path) if exclude else None
+        if exclude_match and exclude_match.group(0):
+            report.path_ignored(child, "matches the --exclude regular expression")
+            continue
+
+        force_exclude_match = (
+            force_exclude.search(normalized_path) if force_exclude else None
+        )
+        if force_exclude_match and force_exclude_match.group(0):
+            report.path_ignored(child, "matches the --force-exclude regular expression")
+            continue
+
+        if child.is_dir():
+            yield from gen_python_files(
+                child.iterdir(),
+                root,
+                include,
+                exclude,
+                force_exclude,
+                report,
+                gitignore,
+            )
+
+        elif child.is_file():
+            include_match = include.search(normalized_path) if include else True
+            if include_match:
+                yield child
+
+
 def check_format_names(string):
     names = string.split(",")
     allowed_names = set(formats.detection_funcs.keys()) - set(["none"])
@@ -22,12 +96,21 @@ def check_format_names(string):
 
 def collect_files(src, include, exclude):
     root = black.find_project_root(tuple(src))
+    gitignore = black.get_gitignore(root)
     report = black.Report()
+
+    force_exclude = ""
 
     for path in src:
         if path.is_dir():
-            yield from black.gen_python_files_in_dir(
-                path, root, include, exclude, report, black.get_gitignore(root),
+            yield from gen_python_files(
+                path.iterdir(),
+                root,
+                include,
+                exclude,
+                force_exclude,
+                report,
+                gitignore,
             )
         elif path.is_file() or str(path) == "-":
             yield path
@@ -172,7 +255,8 @@ def process(args):
         for version in getattr(args, "target_versions", ())
     )
     mode = black.FileMode(
-        line_length=args.line_length, target_versions=target_versions,
+        line_length=args.line_length,
+        target_versions=target_versions,
     )
 
     actions = {
