4619f6f68664e73d919e38b19811fb2ab672ba30
Keewis <keewis@posteo.de>
2020-09-30 23:28:54 +0200

    also ignore force_excluded files in collect_files

--- blackdoc/__main__.py
+++ blackdoc/__main__.py
@@ -37,7 +37,25 @@ def collect_files(src, include, exclude, force_exclude):
                 report,
                 gitignore,
             )
-        elif path.is_file() or str(path) == "-":
+        elif str(path) == "-":
+            yield path
+        elif path.is_file():
+            normalized_path = black.normalize_path_maybe_ignore(path, root, report)
+            if normalized_path is None:
+                continue
+
+            normalized_path = "/" + normalized_path
+            # Hard-exclude any files that matches the `--force-exclude` regex.
+            if force_exclude:
+                force_exclude_match = force_exclude.search(normalized_path)
+            else:
+                force_exclude_match = None
+            if force_exclude_match and force_exclude_match.group(0):
+                report.path_ignored(
+                    path, "matches the --force-exclude regular expression"
+                )
+                continue
+
             yield path
         else:
             print(f"invalid path: {path}", file=sys.stderr)
@@ -171,14 +189,13 @@ def process(args):
         return 2
 
     try:
+        force_exclude = getattr(args, "force_exclude", "")
         force_exclude_regex = (
-            black.re_compile_maybe_verbose(args.force_exclude)
-            if args.force_exclude
-            else None
+            black.re_compile_maybe_verbose(force_exclude) if force_exclude else None
         )
     except black.re.error:
         print(
-            f"Invalid regular expression for force_exclude given: {args.force_exclude!r}",
+            f"Invalid regular expression for force_exclude given: {force_exclude!r}",
             file=sys.stderr,
         )
         return 2
