1c9e60e79c7a95e6ecc6346eb0caa03b1caad7af
Keewis <keewis@posteo.de>
2020-10-30 20:02:00 +0100

    add diff and color diff output
    note: this probably doesn't work on windows.

--- blackdoc/__main__.py
+++ blackdoc/__main__.py
@@ -1,4 +1,6 @@
 import argparse
+import datetime
+import difflib
 import pathlib
 import sys
 
@@ -61,6 +63,43 @@ def collect_files(src, include, exclude, force_exclude):
             print(f"invalid path: {path}", file=sys.stderr)
 
 
+def color_diff(contents):
+    """Inject the ANSI color codes to the diff."""
+    lines = contents.split("\n")
+    for i, line in enumerate(lines):
+        if line.startswith("+++") or line.startswith("---"):
+            line = "\033[1;37m" + line + "\033[0m"  # bold white, reset
+        elif line.startswith("@@"):
+            line = "\033[36m" + line + "\033[0m"  # cyan, reset
+        elif line.startswith("+"):
+            line = "\033[32m" + line + "\033[0m"  # green, reset
+        elif line.startswith("-"):
+            line = "\033[31m" + line + "\033[0m"  # red, reset
+        lines[i] = line
+    return "\n".join(lines)
+
+
+def unified_diff(a, b, path, color):
+    then = datetime.datetime.utcfromtimestamp(path.stat().st_mtime)
+    now = datetime.datetime.utcnow()
+    src_name = f"{path}\t{then} +0000"
+    dst_name = f"{path}\t{now} +0000"
+
+    diff = "\n".join(
+        difflib.unified_diff(
+            a.splitlines(),
+            b.splitlines(),
+            fromfile=src_name,
+            tofile=dst_name,
+            lineterm="",
+        )
+    )
+    if color:
+        diff = color_diff(diff)
+
+    return diff
+
+
 def format_and_overwrite(path, mode):
     try:
         with open(path, mode="rb") as f:
@@ -85,7 +124,7 @@ def format_and_overwrite(path, mode):
     return result
 
 
-def format_and_check(path, mode):
+def format_and_check(path, mode, diff=False, color=False):
     try:
         with open(path, mode="rb") as f:
             content, _, _ = black.decode_bytes(f.read())
@@ -98,6 +137,10 @@ def format_and_check(path, mode):
             result = "unchanged"
         else:
             print(f"would reformat {path}")
+
+            if diff:
+                print(unified_diff(content, new_content, path, color))
+
             result = "reformatted"
     except black.InvalidInput as e:
         print(f"error: cannot format {path.absolute()}: {e}")
@@ -220,10 +263,13 @@ def process(args):
         "inplace": format_and_overwrite,
         "check": format_and_check,
     }
+    action_kwargs = {"diff": args.diff, "color": args.color} if args.diff else {}
 
     action = actions.get(args.action)
 
-    changed_sources = {source: action(source, mode) for source in sources}
+    changed_sources = {
+        source: action(source, mode, **action_kwargs) for source in sources
+    }
     n_reformatted, n_unchanged, n_error = statistics(changed_sources)
 
     report_formatters = {
