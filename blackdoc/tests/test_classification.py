d4d01184e840a6081ed5dc25098be7e1369a8916
Keewis <keewis@posteo.de>
2020-11-03 01:44:46 +0100

    fix the failing classification test

--- blackdoc/tests/test_classification.py
+++ blackdoc/tests/test_classification.py
@@ -1,5 +1,3 @@
-import more_itertools
-
 from blackdoc import classification
 
 from .data import doctest as data
@@ -7,20 +5,16 @@ from .data import print_classification
 
 
 def test_detect_format():
-    lines = enumerate(data.lines, start=1)
+    lines = enumerate(data.lines)
 
     labeled = tuple(classification.detect_format(lines))
 
     print_classification(labeled)
 
-    actual = tuple(max_ - min_ for (min_, max_), _, _ in labeled)
-    expected = data.code_units
+    actual = tuple(range_ for range_, _, _ in labeled)
+    expected = data.line_ranges
     assert expected == actual
 
-    actual = tuple(
-        more_itertools.collapse(
-            [name] * len(lines.split("\n")) for _, name, lines in labeled
-        )
-    )
+    actual = tuple(name for _, name, _ in labeled)
     expected = data.line_labels
     assert expected == actual
