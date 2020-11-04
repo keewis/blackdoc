f0e88f0b6da1e965f342efa39de5f72f0f4a87a7
Keewis <keewis@posteo.de>
2020-11-04 18:06:18 +0100

    add a expected docstring to the doctest test data

--- blackdoc/tests/data/doctest.py
+++ blackdoc/tests/data/doctest.py
@@ -54,3 +54,59 @@ labels = {
     30: "none",
 }
 line_ranges, line_labels = from_dict(labels)
+
+expected = """ a function to open files
+
+    with a very long description
+
+    >>> file = open(
+    ...     "very_long_filepath",
+    ...     mode="a",
+    ... )
+    >>> file
+    <_io.TextIOWrapper name='very_long_filepath' mode='w' encoding='UTF-8'>
+
+    text after the first example, spanning
+    multiple lines
+
+    >>> file.closed
+    False
+
+    >>> def myfunc2(arg1, arg2):
+    ...     '''Docstring for function myfunc2 in docstring
+    ...
+    ...     More description of the function.
+    ...     '''
+    ...     pass
+    ...
+    >>>
+
+    >>> if myfunc2(2, 1) is not None:
+    ...     print("caught")
+    ...
+    >>> a = 2
+"""
+expected_lines = expected.split("\n")
+expected_labels = {
+    1: "none",
+    2: "none",
+    3: "none",
+    4: "none",
+    (5, 9): "doctest",
+    9: "doctest",
+    10: "none",
+    11: "none",
+    12: "none",
+    13: "none",
+    14: "none",
+    15: "doctest",
+    16: "none",
+    17: "none",
+    (18, 25): "doctest",
+    25: "doctest",
+    26: "none",
+    (27, 30): "doctest",
+    30: "doctest",
+    31: "none",
+}
+expected_line_ranges, expected_line_labels = from_dict(expected_labels)
