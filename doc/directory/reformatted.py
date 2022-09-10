dbfb9f13b7a4aaaf1246149b6160f2d3a7dfe7ae
Keewis <keewis@posteo.de>
2022-09-10 02:30:47 +0200

    extend the example doctests

--- doc/directory/reformatted.py
+++ doc/directory/reformatted.py
@@ -21,6 +21,17 @@ doctests:
 ...     "m",
 ... }
 
+>>> s = "a" + "b"
+
+>>> def f():
+...     '''nested docstring
+...
+...     parameter documentation
+...     '''
+...
+...     s = '''triple-quoted string'''
+...
+
 ipython:
 In [1]: d = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7, "i": 8, "j": 9}
 """
