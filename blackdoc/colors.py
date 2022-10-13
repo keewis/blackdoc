750c23e08a0992e518d98d502532c3324ae57ca1
Keewis <keewis@posteo.de>
2022-10-13 16:24:06 +0200

    add a highlighter for diffs using rich

--- blackdoc/colors.py
+++ blackdoc/colors.py
@@ -1,6 +1,9 @@
 import functools
+import itertools
 import re
 import sys
+
+from rich.highlighter import Highlighter
 
 from .blackcompat import wrap_stream_for_windows
 
@@ -85,3 +88,50 @@ def color_diff(contents):
 
     lines = contents.split("\n")
     return "\n".join(colorize_line(line) for line in lines)
+
+
+class DiffHighlighter(Highlighter):
+    def highlight(self, text):
+        def line_style(lineno, line):
+            if line.startswith("+++") or line.startswith("---"):
+                yield lineno, (0, len(line)), "bold white"
+            elif line.startswith("@@"):
+                yield lineno, (0, len(line)), "cyan"
+            elif line.startswith("+"):
+                yield lineno, (0, len(line)), "green"
+            elif line.startswith("-"):
+                yield lineno, (0, len(line)), "red"
+                trailing_whitespace = trailing_whitespace_re.search(line)
+                if trailing_whitespace:
+                    start, end = trailing_whitespace.span()
+                    yield lineno, (start, end), "red on red"
+            else:
+                yield lineno, (0, len(line)), ""
+
+        def line_offsets(text):
+            import re
+
+            line_re = re.compile("\n")
+            matches = line_re.finditer(text)
+
+            return [0] + [m.end() for m in matches]
+
+        def move_span(start, end, offset):
+            return start + offset, end + offset
+
+        def diff_styles(text):
+            lines = text.split("\n")
+            line_styles = itertools.chain.from_iterable(
+                line_style(lineno, line) for lineno, line in enumerate(lines, start=1)
+            )
+
+            offsets = line_offsets(text)
+            styles = {
+                move_span(start, end, offsets[lineno - 1]): style
+                for lineno, (start, end), style in line_styles
+            }
+
+            yield from styles.items()
+
+        for (start, end), style in diff_styles(text.plain):
+            text.stylize(style, start=start, end=end)
