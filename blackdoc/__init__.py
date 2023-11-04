021170cae8cffda897c7ddbd55f3fbe3a95998db
Justus Magin <keewis@posteo.de>
2023-11-04 16:58:04 +0100

    remove the conditional requirement of `importlib-metadata`

--- blackdoc/__init__.py
+++ blackdoc/__init__.py
@@ -1,7 +1,4 @@
-try:
-    from importlib.metadata import version
-except ImportError:
-    from importlib_metadata import version
+from importlib.metadata import version
 
 from .blacken import blacken
 from .classification import detect_format
