b5200c8fd13a80f40e956311a138c1b4e0b8c9dd
Justus Magin <keewis@posteo.de>
2025-10-18 19:07:10 +0200

    backwards compat

--- blackdoc/diff.py
+++ blackdoc/diff.py
@@ -1,10 +1,12 @@
 import datetime
 import difflib
 
+utc = datetime.timezone(datetime.timedelta(hours=0), name="UTC")
+
 
 def unified_diff(a, b, path):
-    then = datetime.datetime.fromtimestamp(path.stat().st_mtime, datetime.UTC)
-    now = datetime.datetime.now(datetime.UTC)
+    then = datetime.datetime.fromtimestamp(path.stat().st_mtime, utc)
+    now = datetime.datetime.now(utc)
     src_name = f"{path}\t{then} +0000"
     dst_name = f"{path}\t{now} +0000"
 
